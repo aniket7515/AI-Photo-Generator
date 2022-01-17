feddab82c7f52400a937d16d8794c43b050c4f04
Aniket <aniketdhokane2071@gmail.com>
2022-01-17 15:50:52 +0530

    COmpleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
+
 function App() {
+ 
+  const[image,setImage]=useState('');
+
+  const handleChange=()=>{
+    // const key=process.env.REACT_APP_API_KEY;
+    // console.log(key);
+    axios.get(
+      `https://api.generated.photos/api/v1/faces?api_key=${process.env.REACT_APP_API_KEY}&order_by=random`
+      
+    ).then(res =>{
+      const url= res.data.faces[0].urls[4][512];
+      url && setImage(url);
+    }).catch(err =>{
+      console.log(err.message);
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>AI Photo Generator</h1>
+      {image && <img src={image} />}
+      <div > <button type="button" onClick={handleChange}>CHANGE IMAGE</button>
+      </div>
+     
     </div>
   );
 }
 
 export default App;
+
+
